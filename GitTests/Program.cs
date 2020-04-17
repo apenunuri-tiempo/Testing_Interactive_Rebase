49384934ebf9d85933ce9e541e07711217734d20
Alfonso Penunuri <lpenunuri@tiempodevelopment.com>
2020-04-17 12:22:09 -0500

    Method 2

--- GitTests/Program.cs
+++ GitTests/Program.cs
@@ -8,12 +8,18 @@ namespace GitTests
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine(Method_Branch1());
+            Console.WriteLine(Method_Branch2());
             Console.ReadLine();
         }
 
         private static string Method_Branch1()
         {
             return "Method from Branch 1";
+        }
+
+        private static string Method_Branch2()
+        {
+            return "Method from Branch 2";
         }
     }
 }
